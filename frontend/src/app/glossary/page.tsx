cf4076c64d5d6324e358d353cd8c60964ef0b203
Orionlul <reillybell9@gmail.com>
2025-10-19 00:13:20 -0700

    3we

--- frontend/src/app/glossary/page.tsx
+++ frontend/src/app/glossary/page.tsx
@@ -6,13 +6,14 @@ type Team = {
 };
 
 export default async function Page() {
-      const response = await fetch('http://backend:8000/teams');
+      const response = await fetch('http://backend:8000/glossary');
  const teams: Team[] = await response.json ();
 
   return (
     <div>
-        <h1 className="text-7xl absolute top-16 left-110 ">Stupid Dumb Fetched Glossary</h1>
-        <h2 className="text-4xl absolute top-34 left-110">But Tanner, isnt it static text? "Yes, but what if I want to change it?"</h2>
+      <div className=" bg-red-100">
+        <h1 className=" text-7xl absolute top-16 left-110 ">Stupid Dumb Fetched Glossary</h1>
+        <h2 className=" text-4xl absolute top-34 left-110 ">But Tanner, isnt it static text? "Yes, but what if I want to change it?"</h2>
         <Image
           src="/Dumbjared-glossary.png"
           alt="DumbJared"
@@ -20,7 +21,7 @@ export default async function Page() {
           height={150}
           className="z-0 flex rotate-8 justify-start"
 />
-
+      </div>
         <div className="text-4xl my-12 relative mb-52">
           <ul className="list-disc space-y-4 pl-5">
               {teams.map((team, index) => (
